d6efc109bb9f8b815cdd95dfe2e910b9e0c26d40
Arimazz <bodyaprymyak@icloud.com>
2020-02-12 00:15:26 +0200

    Updates

---
+++ precache-manifest.2fee835016fdfac9afe346eff9d692a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e6e7fe3f51df1febb1a7d2af632e671",
+    "url": "/react_decompose/index.html"
+  },
+  {
+    "revision": "e41efc15958594efaecf",
+    "url": "/react_decompose/static/css/main.8b7f8c41.chunk.css"
+  },
+  {
+    "revision": "5fdac0a2c9586d9d1584",
+    "url": "/react_decompose/static/js/2.6b91d4b6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_decompose/static/js/2.6b91d4b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e41efc15958594efaecf",
+    "url": "/react_decompose/static/js/main.796ca357.chunk.js"
+  },
+  {
+    "revision": "c2ab4782cd315247655b",
+    "url": "/react_decompose/static/js/runtime-main.14716fab.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9bb6061e1f034dfe77e397265b9db3f9.js"
+  "/react_decompose/precache-manifest.2fee835016fdfac9afe346eff9d692a7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_decompose/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
